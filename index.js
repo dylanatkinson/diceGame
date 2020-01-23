a01dd15cad88ca413e783c2aa184a0fc0adf6c87
dylanatkinson <59877213+dylanatkinson@users.noreply.github.com>
2020-01-23 16:11:21 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ diceImage.style.visibility = "hidden";
 // rolls the dice to give a random number between 1 and 6
 const rollDice = () => {
     numberRolled = Math.ceil(Math.random() * 6);
-    // console.log(numberRolled);
+    console.log(`The random roll is ${numberRolled}`);
 };
 
 // if statement to display the message if you win/lose
 const winLoss = () => {
     if (numberRolled == 1) {
-        winOrLose.textContent = "You lose";
+        winOrLose.textContent = "You lose.";
         winOrLose.style.visibility = "visible";
         points = 0;
         rollButton.textContent = "Try Again?";
@@ -31,7 +31,6 @@ const winLoss = () => {
         points = 0;
         rollButton.textContent = "Play Again?";
     } else {
-        logScore();
         rollButton.textContent = "Click to Roll";
     };
 };
@@ -39,6 +38,7 @@ const winLoss = () => {
 // function to add the number rolled by the dice to the points scored by the player
 const logScore = () => {
     points += numberRolled;
+    console.log(`Then added to the points is ${points}`);
 };
 
 // event listener to perform functions when the button is clicked
@@ -47,11 +47,11 @@ rollButton.addEventListener("click", () => {
     winOrLose.style.visibility = "hidden";
     // console.log("button clicked");
     rollDice();
-    playerScore.textContent = numberRolled;
+    playerScore.textContent = `Dice Roll: ${numberRolled}`;
     diceImage.style.visibility = "visible";
     // changes the dice image to the one matching the number rolled
     diceImage.src = `img/dice${numberRolled}.png`;
     logScore();
-    currentScore.textContent = points;
+    currentScore.textContent = `Current Score: ${points}`;
     winLoss();
 });
